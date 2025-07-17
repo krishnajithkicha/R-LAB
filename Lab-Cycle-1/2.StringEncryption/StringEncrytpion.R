1af6bf5b7985876ca64d1314810d90bedc8406e5
Krishna Ajith <142655890+krishnajithkicha@users.noreply.github.com>
2025-07-17 15:30:24 +0530

    Delete Lab-Cycle-1/2.StringEncryption directory

--- Lab-Cycle-1/2.StringEncryption/StringEncrytpion.R
+++
@@ -1,5 +0,0 @@
-sentence <- readLines(prompt="Enter your sentence:")
-print(sentence)
-cleaned_sentence <- unlist(strsplit(gsub("[[:punct:]]","",sentence),"\\s+"))
-print(cleaned_sentence)
-encryption
