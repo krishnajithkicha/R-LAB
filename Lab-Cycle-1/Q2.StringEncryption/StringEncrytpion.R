3a03412743297ff88c3948b086a8d15e728280ae
Krishna Ajith <142655890+krishnajithkicha@users.noreply.github.com>
2025-09-10 14:26:50 +0530

    Update StringEncrytpion.R

--- Lab-Cycle-1/Q2.StringEncryption/StringEncrytpion.R
+++ Lab-Cycle-1/Q2.StringEncryption/StringEncrytpion.R
@@ -1,5 +1,22 @@
-sentence <- readLines(prompt="Enter your sentence:")
-print(sentence)
-cleaned_sentence <- unlist(strsplit(gsub("[[:punct:]]","",sentence),"\\s+"))
-print(cleaned_sentence)
-encryption
+#sentence <- "readline(prompt="Enter a sentence:")"
+#shift <- as.integer(readline(prompt="Enter a shift value:"))
+sentence <- "Hello World"
+shift <-3
+cleaned_sentence <- gsub("[[:punct:]]","",sentence)
+n <-nchar(cleaned_sentence)
+ascii_value <- as.integer(charToRaw(cleaned_sentence))
+
+
+for (i in 1:n){
+  if (ascii_value[i]!=32){
+    if(ascii_value[i]>=65 && ascii_value[i] <=90){
+      ascii_value[i] <- ((ascii_value[i]-65+shift)%%26)+65
+    }
+    if(ascii_value[i]>=97 && ascii_value[i] <=122){
+      ascii_value[i] <- ((ascii_value[i]-97+shift)%%26)+97
+    }
+  }
+}
+encrypted_sentence <- rawToChar(as.raw(ascii_value))
+cat("Sentence:",sentence,"\n")
+cat("Encrypted Sentence:",encrypted_sentence,"\n")
